107ba0d8aa0f9b8f902d7e30c5336c496351edf1
Brian Cui <brian.cui@nxvitech.com>
2025-06-19 18:12:00 +0800

    服务器

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://article-backend-ten.vercel.app/',
         changeOrigin: true
       },
       '^/plagwise/.*': {
